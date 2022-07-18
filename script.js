b14230ba0c84e331d9da31fc52931682010c718f
Alex CZM <alexczm@proton.me>
2022-07-18 19:02:14 +0300

    Add simple UI

--- script.js
+++ script.js
@@ -9,56 +9,82 @@ const ENUM = {
 
 let userWinsCount = 0;
 let computerWinsCount = 0;
+let matchNo = 0;
+
+const container = document.querySelector("#title");
+const buttons = document.createElement("div");
+
+function addButtons() {
+    const rockButton = document.createElement("button");
+    rockButton.innerText = "Rock";
+    rockButton.addEventListener('click', (e) => playGame(e));
+
+    const paperButton = document.createElement("button");
+    paperButton.innerText = "Paper";
+    paperButton.addEventListener('click', (e) => playGame(e));
+
+    const scissorButton = document.createElement("button");
+    scissorButton.innerText = "Scissor";
+    scissorButton.addEventListener('click', (e) => playGame(e));
+
+    buttons.appendChild(rockButton);
+    buttons.appendChild(paperButton);
+    buttons.appendChild(scissorButton);
+
+    container.appendChild(buttons)
+}
+
+const playGame = (event) => {
+    let playerChoose = event.target.innerText;
+    playerChoose = playerChoose.toLowerCase();
+    let computerChoice = getComputerChoice();
+    matchNo ++;
+    //print on display
+    container.appendChild(printChoices(playerChoose, computerChoice, matchNo))
+    playRound(playerChoose, computerChoice);
+}
 
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3);
     return CHOICES[random];
 }
-function getUserChoice() {
-    let userChoice ;
-    while (!CHOICES.includes(userChoice)) {
-        userChoice = prompt("Please insert a valid choice: rock, paper, scissor");
-        userChoice = userChoice.toLowerCase();
-    } 
-    return userChoice;
+
+function printChoices(user, computer, counter) {
+    const result = document.createElement("div");
+    const choices = document.createElement("p");
+    choices.innerHTML = `Match number ${matchNo}: <br>User choice: <b>${user}</b><br>Computer choice: <b>${computer}</b>`;
+    result.appendChild(choices);
+    return result;
 }
 
 function playRound(userChoice, computerChoice) {
-    console.log(`User choice: ${userChoice} \nComputer choice: ${computerChoice}`);
-    // rock -> scissor -> paper -> again rock
-    if(userChoice === computerChoice) {
-        return "draw"
-    }
-    
     let result = ENUM[userChoice] - ENUM[computerChoice];
-    console.log("result: ",result)
-    if (result === 1) return "User won";
-    else if(result === 2) return "Pc won";
-    else if(result === -1) return "Pc won";
-    else if(result === -2) return "User won";
-    else return;
-}
 
-/**find the best out of 5 rounds */
-function game() {
-    for(let round = 0; round < 5; round ++) {
-        console.log(`Round ${round}: `)
-        let computerChoice = getComputerChoice();
-        let userChoice = getUserChoice();
-        result  = playRound(userChoice, computerChoice)
-        console.log(`${result} this round.`);
-
-        if(result === "User won") {
-            userWinsCount ++;
-        } else if(result === "Pc won") {
-            computerWinsCount++;
-        }
+    if (result === 1 || result === -2) {
+        userWinsCount ++
+    } else if (result === 2 || result === -1){
+        computerWinsCount++;
     }
-    if(userWinsCount > computerWinsCount) {
-        console.log("User won the GAME");
-    } else if(userWinsCount < computerWinsCount) {
-        console.log("Pc won the GAME")
-    } else console.log("It's a draw")
+    if(userWinsCount == 5) {
+        buttons.prepend(printFinalResult("user"));
+    }
+    else if(computerWinsCount == 5) {
+        buttons.prepend(printFinalResult("PC"));
+    }
 }
 
-game();
+
+function resetGame() {
+    userWinsCount = 0;
+    computerWinsCount = 0;
+    matchNo = 0;
+}
+
+function printFinalResult(win) {
+    const winner = document.createElement("p");
+    winner.innerText = `Winner is ${win}`;
+    return winner;
+}
+
+//render button choices
+addButtons();
