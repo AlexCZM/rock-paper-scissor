17a0f22e081d87a6578e13de13663258840d6e30
Alex CZM <alexczm@proton.me>
2022-07-19 00:58:25 +0300

    Rework entire logic

--- script.js
+++ script.js
@@ -1,90 +1,112 @@
+const CHOICES = ["rock", "paper", "scissor"];
+let playerChoice = '';
 
-const CHOICES = ["rock", "paper", "scissor"]; 
+let playerWins = 0;
+let computerWins = 0;
 
-const ENUM = {
-    rock: 1,
-    paper: 2,
-    scissor: 3
-}
+const pageContainer = document.querySelector("#pageContent");
+const winnerDiv = document.createElement("div");
+//player and comp scores
+const scoreDiv = document.createElement("div");
+//player and comp current choices
+const choices  = document.createElement("p")
+pageContainer.appendChild(choices)
 
-let userWinsCount = 0;
-let computerWinsCount = 0;
-let matchNo = 0;
-
-const container = document.querySelector("#title");
-const buttons = document.createElement("div");
-
-function addButtons() {
-    const rockButton = document.createElement("button");
-    rockButton.innerText = "Rock";
-    rockButton.addEventListener('click', (e) => playGame(e));
-
-    const paperButton = document.createElement("button");
-    paperButton.innerText = "Paper";
-    paperButton.addEventListener('click', (e) => playGame(e));
-
-    const scissorButton = document.createElement("button");
-    scissorButton.innerText = "Scissor";
-    scissorButton.addEventListener('click', (e) => playGame(e));
-
-    buttons.appendChild(rockButton);
-    buttons.appendChild(paperButton);
-    buttons.appendChild(scissorButton);
-
-    container.appendChild(buttons)
-}
-
-const playGame = (event) => {
-    let playerChoose = event.target.innerText;
-    playerChoose = playerChoose.toLowerCase();
-    let computerChoice = getComputerChoice();
-    matchNo ++;
-    //print on display
-    container.appendChild(printChoices(playerChoose, computerChoice, matchNo))
-    playRound(playerChoose, computerChoice);
-}
 
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3);
     return CHOICES[random];
 }
 
-function printChoices(user, computer, counter) {
-    const result = document.createElement("div");
-    const choices = document.createElement("p");
-    choices.innerHTML = `Match number ${matchNo}: <br>User choice: <b>${user}</b><br>Computer choice: <b>${computer}</b>`;
-    result.appendChild(choices);
-    return result;
+function renderPage() {
+    // container div
+    const content = document.createElement("div");
+    //buttons container
+    const buttons = document.createElement("div");
+    //score
+    const playersScore = document.createElement("p");
+    //create 3 choices 
+    const btnRock = document.createElement("button");
+    btnRock.innerText = "Rock";
+    btnRock.addEventListener('click', (e) => setPlayerChoice (e));
+    pageContainer.appendChild(btnRock);
+
+    const btnPaper = document.createElement("button");
+    btnPaper.innerText = "Paper";
+    btnPaper.addEventListener('click', (e) => setPlayerChoice (e));
+    pageContainer.appendChild(btnPaper);
+
+    const btnScissor = document.createElement("button");
+    btnScissor.innerText = "Scissor";
+    btnScissor.addEventListener('click', (e) => setPlayerChoice (e));
+    pageContainer.appendChild(btnScissor);
 }
 
-function playRound(userChoice, computerChoice) {
-    let result = ENUM[userChoice] - ENUM[computerChoice];
+function setPlayerChoice(event) {
+    let compChoice = getComputerChoice();
+    playerChoice = event.target.innerText;
+    playerChoice = playerChoice.toLowerCase();
+    console.log(playerChoice)
+    console.log(compChoice)
+    //render player and computer choices
+    choices.innerHTML = `
+        Player choice: ${playerChoice}
+        <br>Computer chice: ${compChoice}
+        `;
+    getRoundWinner(playerChoice, compChoice);
+    getGlobalWinner();
+}
 
-    if (result === 1 || result === -2) {
-        userWinsCount ++
-    } else if (result === 2 || result === -1){
-        computerWinsCount++;
+function getRoundWinner(player, computer) {
+    let draw = false;
+    if (player === computer) draw = true;
+    else if (player === "rock") {
+        computer === "paper" ? computerWins++ : playerWins++;   
     }
-    if(userWinsCount == 5) {
-        buttons.prepend(printFinalResult("user"));
+    else if(player === "paper") {
+        computer === "scissor" ? computerWins++ : playerWins++;
     }
-    else if(computerWinsCount == 5) {
-        buttons.prepend(printFinalResult("PC"));
+    else if(player === "scissor") {
+        computer === "rock" ? computerWins++ : playerWins++;
     }
+
+    if(draw && computerWins === 0 && playerWins === 0) {
+        scoreDiv.innerHTML = "<p>It's a draw</p>"
+    } else {
+        scoreDiv.innerHTML = `
+        Player score: ${playerWins}
+        <br>Computer score: ${computerWins}
+    `;
+    }
+
+    pageContainer.appendChild(scoreDiv);
 }
 
+function getGlobalWinner() {
+    let winner = "";
+    if (playerWins == 5) winner = "Player";
+    else if(computerWins == 5) winner = "Computer";
+    pageContainer.appendChild(winnerDiv)
 
-function resetGame() {
-    userWinsCount = 0;
-    computerWinsCount = 0;
-    matchNo = 0;
+    //do we have a winner?
+    if(winner !== "") {
+        winnerDiv.innerText = `${winner} won the game!`;
+        //render new btn to start new game
+        const restart = document.createElement("button");
+        restart.innerText = "Start New Game";
+        restart.addEventListener("click", startNewGame);
+        pageContainer.appendChild(restart);
+    }
+    
+}
+function startNewGame() {
+    playerChoice = '';
+    playerWins = 0;
+    computerWins = 0;
+    while(pageContainer.firstChild) {
+        pageContainer.removeChild(pageContainer.firstChild);
+    }
+    renderPage();
 }
 
-function printFinalResult(win) {
-    const winner = document.createElement("p");
-    winner.innerText = `Winner is ${win}`;
-    return winner;
-}
-
-//render button choices
-addButtons();
+startNewGame();
